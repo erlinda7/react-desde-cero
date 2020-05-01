42672c0a2e64217270b8399003a171f601c44626
Erlinda <chambimanzanoerlinda@gmail.com>
2020-05-01 14:40:23 -0400

    codigo realizado en la clase 3 de edteam

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,38 +1,35 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Curso = () => (
+const Curso = ({ title, image, price, profesor }) => (
   <article className="card">
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img
-        src="https://edteam-media.s3.amazonaws.com/courses/original/565fa9ec-8f84-448a-b0d8-855d059f16dd.png"
-        alt="poster del curso"
-      />
+      <img src={image} alt={title} />
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-      </h3>
-      <div className="s-mb-2 s-main-center">
-        <div className="card__teacher s-cross-center">
-          <div className="card__avatar s-mr-1">
-            <div className="circle img-container">
-              <img
-                src="https://edteam-media.s3.amazonaws.com/users/thumbnail/f2db2437-1baf-481d-9f3a-04ba848008a7.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <span className="small">Alvaro Felipe</span>
-        </div>
-      </div>
+      <h3 className="t5 s-mb-2 s-center">{title ? title : "No hay titulo"}</h3>
+      <div className="s-mb-2 s-main-center">{profesor}</div>
       <div className="s-main-center">
         <a className="button--ghost-alert button--tiny" href="#">
-          $ 20USD
+          {`$ ${price} USD`}
         </a>
       </div>
     </div>
   </article>
 );
 
+Curso.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number,
+  profesor: PropTypes.string,
+};
 
+Curso.defaultProps = {
+  title: "No se encontro titulo",
+  image:
+    "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/480x272/public/media/image/2019/01/default_wallpaper.jpeg?itok=RGGJXij8",
+  price: "__",
+  profesor: "",
+};
 export default Curso;
