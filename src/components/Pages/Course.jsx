1359f9e3a4bfd1647c7e1bd5a99e14348079bfd8
Erlinda <chambimanzanoerlinda@gmail.com>
2020-05-03 16:07:15 -0400

    como actualizar o modificar el  state usando useState sin ser componente de clase

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -2,29 +2,31 @@ import React, { useState } from "react";
 
 const Course = ({ match }) => {
   const [state, setState] = useState({
-    cursoActual: {
-      id: 1,
-      title: "react desde cero",
-      image:
-        "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
-      price: 30,
-      profesor: "Beto Quiroga",
-    },
+    id: 1,
+    title: "react desde cero",
+    image:
+      "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
+    price: 30,
+    profesor: "Beto Quiroga",
   });
 
-  console.log(state);
-  
+  const changeTitle = (text) => {
+    setState({
+      ...state,
+      title: text,
+    });
+  };
+
   return (
     <div className="ed-grid m-grid-3">
-      {state.cursoActual ? (
+      {state ? (
         <>
-          <h1 className="m-cols-3"> {state.cursoActual.titulo} </h1>
-          <img
-            className="m-cols-1"
-            src={state.cursoActual.image}
-            alt={state.cursoActual.titulo}
-          />
-          <p className="m-cols-2">Profesor: {state.cursoActual.profesor} </p>
+          <h1 className="m-cols-3"> {state.title} </h1>
+          <img className="m-cols-1" src={state.image} alt={state.title} />
+          <p className="m-cols-2">Profesor: {state.profesor} </p>
+          <button onClick={changeTitle.bind(this, "Go desde cero")}>
+            Cambiar Titulo
+          </button>
         </>
       ) : (
         <h1>El curso no existe</h1>
