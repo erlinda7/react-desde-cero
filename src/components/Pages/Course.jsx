a1247a191ad922aff968ab2bc5f42c42377b5e92
Erlinda <chambimanzanoerlinda@gmail.com>
2020-05-03 16:20:36 -0400

    useEffect que se nos permite emular el ciclo de vida del componente

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -1,22 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Course = ({ match }) => {
-  const [state, setState] = useState({
-    id: 1,
-    title: "react desde cero",
-    image:
-      "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
-    price: 30,
-    profesor: "Beto Quiroga",
-  });
+  const [state, setState] = useState({});
 
-  const changeTitle = (text) => {
-    setState({
-      ...state,
-      title: text,
-    });
-  };
-
+  useEffect(() => {
+    axios
+      .get("http://my-json-server.typicode.com/erlinda7/json-db/cursos/4")
+      .then((resp) => setState(resp.data));
+  },[]);
   return (
     <div className="ed-grid m-grid-3">
       {state ? (
@@ -24,9 +16,6 @@ const Course = ({ match }) => {
           <h1 className="m-cols-3"> {state.title} </h1>
           <img className="m-cols-1" src={state.image} alt={state.title} />
           <p className="m-cols-2">Profesor: {state.profesor} </p>
-          <button onClick={changeTitle.bind(this, "Go desde cero")}>
-            Cambiar Titulo
-          </button>
         </>
       ) : (
         <h1>El curso no existe</h1>
