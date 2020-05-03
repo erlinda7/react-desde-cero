70117836ed509a3bf1a2f327a00297995dfb31b7
Erlinda <chambimanzanoerlinda@gmail.com>
2020-05-03 11:37:57 -0400

    agregando logica al HOC

--- src/components/HOC/withLoader.js
+++ src/components/HOC/withLoader.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react"
 
-const withLoader = (WrappedComponent) => {
+const withLoader = (propValue, WrappedComponent) => {
     return class WithLoader extends Component {
         constructor(props) {
             super(props)
         }
 
         render() {
-            return <WrappedComponent {...this.props} />
+
+            // return this.props.courses.length === 0
+            return this.props[propValue].length === 0
+                ? <h1 className="t3">Cargando .....</h1>
+                : <WrappedComponent {...this.props} />
         }
     }
 }
--- src/components/Organisms/CourseGrid.jsx
+++ src/components/Organisms/CourseGrid.jsx
@@ -3,22 +3,18 @@ import React from "react";
 import CursoCard from "../Molecules/CourseCard";
 import withLoader from "../HOC/withLoader";
 
-const CourseGrid = ({courses}) => (
+const CourseGrid = ({ courses }) => (
   <div className="ed-grid m-grid-4">
-    {courses.length === 0 ? (
-      <h1 className="t3">Cargando...</h1>
-    ) : (
-      courses.map((c) => (
-        <CursoCard
-          key={c.id}
-          id={c.id}
-          title={c.title}
-          image={c.image}
-          price={c.price}
-          professor={c.profesor}
-        />
-      ))
-    )}
+    {courses.map((c) => (
+      <CursoCard
+        key={c.id}
+        id={c.id}
+        title={c.title}
+        image={c.image}
+        price={c.price}
+        professor={c.profesor}
+      />
+    ))}
   </div>
 );
-export default  withLoader(CourseGrid) ;
+export default withLoader("courses", CourseGrid);
