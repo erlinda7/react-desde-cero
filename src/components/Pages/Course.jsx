5ce7c91c5ebc865f29e00e942aa3dec78b5d70c6
Erlinda <chambimanzanoerlinda@gmail.com>
2020-05-03 16:34:48 -0400

    multiples states

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -2,21 +2,40 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Course = ({ match }) => {
-  const [state, setState] = useState({});
+  const [course, setCourse] = useState({});
+  const [comment, setComment] = useState("Sin comentarios"); //como no sera un objeto
 
   useEffect(() => {
     axios
-      .get("http://my-json-server.typicode.com/erlinda7/json-db/cursos/4")
-      .then((resp) => setState(resp.data));
-  },[]);
+      .get(
+        `http://my-json-server.typicode.com/erlinda7/json-db/cursos/${match.params.id}`
+      )
+      .then((resp) => setCourse(resp.data));
+  }, []);
+
+  const myComment = (e) => {
+    setComment(e.target.value);
+  };
+
   return (
     <div className="ed-grid m-grid-3">
-      {state ? (
-        <>
-          <h1 className="m-cols-3"> {state.title} </h1>
-          <img className="m-cols-1" src={state.image} alt={state.title} />
-          <p className="m-cols-2">Profesor: {state.profesor} </p>
-        </>
+      {course ? (
+        <div>
+          <div className="l-block">
+            <h1 className="m-cols-3"> {course.title} </h1>
+            <img className="m-cols-1" src={course.image} alt={course.title} />
+            <p className="m-cols-2">Profesor: {course.profesor} </p>
+          </div>
+          <div className="ed-grid">
+            <h2>Escribe tu comentario</h2>
+            <input
+              type="text"
+              placeholder="Escribe...."
+              onChange={myComment.bind(this)}
+            />
+            <p>{comment}</p>
+          </div>
+        </div>
       ) : (
         <h1>El curso no existe</h1>
       )}
